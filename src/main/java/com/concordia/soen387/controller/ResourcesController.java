9470458aa06825412468f033afed5bfc9b36ebc8
EthanShen8189 <sy8189@gmail.com>
2016-11-06 14:42:10 -0500

    resource controller&service class created

---
+++ src/main/java/com/concordia/soen387/controller/ResourcesController.java
@@ -0,0 +1,11 @@
+package com.concordia.soen387.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by EthanShen on 2016-11-06.
+ */
+@Controller
+public class ResourcesController {
+
+}
---
+++ src/main/java/com/concordia/soen387/service/ResourcesService.java
@@ -0,0 +1,7 @@
+package com.concordia.soen387.service;
+
+/**
+ * Created by EthanShen on 2016-11-06.
+ */
+public class ResourcesService {
+}
