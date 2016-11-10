87bd7bf659d84d653ea67413b318624c905f7e7d
Van Do <van_do@live.com>
2016-11-09 22:46:29 -0500

    added the structure of the servlet to handle the post requests of inventory ui

--- src/main/java/com/concordia/soen387/controller/LoginController.java
+++ src/main/java/com/concordia/soen387/controller/LoginController.java
@@ -1,25 +1,37 @@
 package com.concordia.soen387.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * Created by Yang Shen on 2016-11-01.
  */
 @Controller
+@SessionAttributes({"username" , "userType"})
 public class LoginController {
+	
+	
 
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String indexPage(){
         return "index";
     }
 
-    @RequestMapping(value="/invLogin", method=RequestMethod.POST, params = {"username", "password"})
+    @RequestMapping(value="/invLogin", method=RequestMethod.POST, params = {"username", "password", "userType"})
     public void loginValidation(@RequestParam String username,
-                                        @RequestParam String password){
-
+                                        @RequestParam String password,
+                                        	@RequestParam String userType,
+                                        		ModelMap model){
+    	// validate user from database
+    	
+    	//store username and userType in the model to keep track of sessions
+    	model.put("username", username);
+    	model.put("userType", userType);
+    	
     }
 
 }
--- src/main/java/com/concordia/soen387/controller/ResourcesController.java
+++ src/main/java/com/concordia/soen387/controller/ResourcesController.java
@@ -1,11 +1,58 @@
 package com.concordia.soen387.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by EthanShen on 2016-11-06.
  */
 @Controller
 public class ResourcesController {
+	
+	
+	 //change password
+	 @RequestMapping(value="/changePw", method=RequestMethod.POST, params = {"oldPassword", "newPassword", "confirmPassword"})
+	    public void changePassword(@RequestParam String oldPassword, @RequestParam String newPassword, @RequestParam String confirmPassword){
+	    	
+		 
+	    }
+	
+	 //add computer
+	 @RequestMapping(value="/addComp", method=RequestMethod.POST, params = {"machineType", "hostName" , "operatingSystem" , "manufactuerer", "model",
+			"wirelessnetworking", "wirednetworking" , "speakersincluded", "keyboardincluded", "mouseincluded", "hdmiout" })
+	    public void addComputer(@RequestParam String machineType, @RequestParam String hostName, @RequestParam String operatingSystem,
+	    		@RequestParam String manufacturer, @RequestParam String model, @RequestParam String wirelessnetworking, @RequestParam String wirednetworking,
+	    		@RequestParam String speakersincluded, @RequestParam String keyboardincluded, @RequestParam String mouseincluded, @RequestParam String hdmiout){
+	    	
+
+	    }
+	 
+	 //add projector
+	 @RequestMapping(value="/addProj", method=RequestMethod.POST, params = {"projectorHeight", "projectorWidth", "projectorRoomNumber", "hdmiin",
+			 																"dviin", "vgain" })
+	    public void addProjector(@RequestParam String projectorHeight, @RequestParam String projectorWidth, @RequestParam String projectorRoomNumber, @RequestParam String hdmiin,
+	    		@RequestParam String dviin, @RequestParam String vgain){
+	    	
+
+	    }
+	 
+	 
+	 //add room
+	 //jesse's task
+	 
+	 
+	 //add board
+	 //jesse's task
+	 
+	 
+	 
+	 //edit item
+	 @RequestMapping(value="/changeItem", method=RequestMethod.POST, params = {"itemName", "status", "description"})
+	    public void editItem(@RequestParam String itemName, @RequestParam String status, @RequestParam String description){
+	    	
+
+	    }
 
 }
